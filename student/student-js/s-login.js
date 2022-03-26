03bd555691577462254c5cdeebcc53454e821c8c
RonxldWilson <ronaldwilson00000@gmail.com>
2022-03-26 09:22:19 +0530

    Created a seperate dir for modules

--- student/student-js/s-login.js
+++
@@ -1,5 +0,0 @@
-main = document.getElementById("main");
-
-$.getJSON('https://cc95-2402-3a80-d22-969a-1843-da9e-feb8-f07b.ngrok.io/Technoboard/student-list.json', function(data) {
-    main.innerHTML += data;
-});
