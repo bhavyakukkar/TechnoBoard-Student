897f980fd216be7927e73e4c863992a8ec3e47d9
bhavyakukkar <nigelwithrow78@gmail.com>
2022-04-05 22:18:56 +0530

    update search-request.php to fully functional

--- student/s-ATS/server/s-ATS-search-request.php
+++ student/s-ATS/server/s-ATS-search-request.php
@@ -1,5 +1,56 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<style>
+		#publish {
+			display: none;
+		}
+	</style>
+</head>
+<body>
 <?php
-		
-		if
+
+	$data_directory = "../../database/";
+
+	function loginTeacher($teacher_id) {
+		$teacher_hash = hash('md5', "Technoboard".$teacher_id."194021119402241940261");
+		$teacher_directory_name = "t-".$teacher_hash;
+
+		return $teacher_directory_name."/";
+	}
+
+	function loginClass($teacher_key, $class_id) {
+		$class_directory_name = "c-".$class_id;
+
+		return $teacher_key.$class_directory_name."/";
+	}
+
+	function findPortal($class_key) {
+		global $data_directory;
+
+		$portal_path = $data_directory.$class_key."portal.json";
+		$portal_json = file_get_contents($portal_path);
+		$portal = json_decode($portal_json, true);
+
+		return $portal;
+	}
+
+	if( !empty($_GET['t']) and !empty($_GET['c']) ) {
+		$teacher_id = $_GET['t'];
+		$class_id = $_GET['c'];
+
+		$portal = findPortal(
+			loginClass(
+				loginTeacher($teacher_id),
+				$class_id
+			)
+		);
+
+		if($portal['publish'] == "true")
+			print '<div id="publish">True</div>';
+		else
+			print '<div id="publish">False</div>';
+	}
+?>
+</body>
+</html>
