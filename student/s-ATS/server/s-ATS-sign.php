a117b43ebb100b01cfaf32ca78642abfced5ab77
bhavyakukkar <nigelwithrow78@gmail.com>
2022-04-05 16:06:22 +0530

    update sign.php to fully functional

--- student/s-ATS/server/s-ATS-sign.php
+++ student/s-ATS/server/s-ATS-sign.php
@@ -1,35 +1,65 @@
 <!DOCTYPE html>
 <html>
 <?php
+		$data_directory = "../../database/";
 		
-		function currentSession($session_hash) {
+		function loginTeacher($teacher_id) {
+			$teacher_hash = hash('md5', "Technoboard".$teacher_id."194021119402241940261");
+			$teacher_directory_name = "t-".$teacher_hash;
 
-            $meta_file_name = "./meta".$session_hash.".json";
-			$meta_json = file_get_contents($meta_file_name);
-			$meta_array = json_decode($meta_json, true);
-			
-			return $meta_array['SessionHash'];
-        }
+			return $teacher_directory_name."/";
+		}
 
-		function addStudentSignature($session_hash, $reg_no) {
+		function loginClass($teacher_key, $class_id) {
+			$class_directory_name = "c-".$class_id;
+
+			return $teacher_key.$class_directory_name."/";
+		}
+
+		function findPortal($class_key) {
+			global $data_directory;
+
+			$portal_path = $data_directory.$class_key."portal.json";
+			$portal_json = file_get_contents($portal_path);
+			$portal = json_decode($portal_json, true);
+
+			return $portal;
+		}
+
+		function loginSession($class_key, $portal) {
+			$session_id = $portal['sessionID'];
+			$session_directory_name = "s-".$session_id;
+
+			return $class_key.$session_directory_name."/";
+		}
+
+		function loginRequest($session_key, $portal) {
+			$request_id = $portal['requestID'];
+			$request_file_name = "r-".$request_id.".json";
 			
-			$session_file_name = "session".$session_hash.".json";
-			$existing_json = file_get_contents($session_file_name);
+			return $session_key.$request_file_name;
+		}
+
+		function signRequest($request_key, $student_id) {
+			global $data_directory;
+
+			$path = $data_directory.$request_key;
+			$existing_json = file_get_contents($path);
 			$existing_array = json_decode($existing_json, true);
 
 			$existing_length = count($existing_array);
 			$id = $existing_length;
 			
-			if(!studentAlreadyResponded($existing_array, $reg_no)) {
+			if(newSignature($student_id, $existing_array)) {
 				
 				$updated_array = $existing_array;
 				$updated_array[$id] = Array (
 					"ID" => strval($id + 1),
-					"RegNo" => strval($reg_no)
+					"StudentID" => strval($student_id)
 				);
 				$updated_json = json_encode($updated_array);
 				
-				if (file_put_contents($session_file_name, $updated_json))
+				if (file_put_contents($path, $updated_json))
 					echo "Success";
 				else
 					echo "Failed to write to file";
@@ -39,39 +69,37 @@
 			}
 		}
 
-		function studentUniqueHash($student_regno, $session_hash) {
-			$hash = hash("md5", $student_regno."Technoboard194021119402241940261".$session_hash);
-			
-			return $hash;
-		}
-
-		function studentAlreadyResponded($existing_array, $reg_no) {
+		function newSignature($student_id, $existing_array) {
 			for($i = 0; $i < count($existing_array); $i++) {
-				if ($existing_array[$i]["RegNo"] == $reg_no)
-					return true;
+				if ($existing_array[$i]["StudentID"] == $student_id)
+					return false;
 			}
-			return false;
+			return true;
 		}
 
-		if(!empty($_GET['r']) and !empty($_GET['h']) and !empty($_GET['s'])) {
-			$reg_no = $_GET['r'];
-			$received_hash = $_GET['h'];
-			$session_hash = $_GET['s'];
+		if(!empty($_GET['id']) and !empty($_GET['t']) and !empty($_GET['c'])) {
+			$student_id = $_GET['id'];
+			$teacher_id = $_GET['t'];
+			$class_id = $_GET['c'];
 			
-			if(isCurrentSession($session_hash) && studentUniqueHash($reg_no, $session_hash) == $received_hash) {
-				addStudentSignature($session_hash, $reg_no);
-			}
-			else {
-				echo "Wrong Session or Wrong Hash";
-			}
+			$class = loginClass(
+				loginTeacher($teacher_id),
+				$class_id
+			);
+
+			$portal = findPortal($class);
+
+			$request = loginRequest(
+				loginSession(
+					$class,
+					$portal
+				),
+				$portal
+			);
+			signRequest($request, $student_id);
 		}
 ?>
 <head>
-	<style>
-		table, th, td {
-			border: 1px solid black;
-		}
-	</style>
 </head>
 
 <body>
