3940d0784aa99cb745f55304fb14664899b990c8
RonxldWilson <ronaldwilson00000@gmail.com>
2022-04-21 14:34:40 +0530

    Revert "changes in ATS"
    This reverts commit ca5b6be8ffceef36acab7fa7900f955c5084648c.

--- student/s-ATS/client/s-ATS-update.js
+++ student/s-ATS/client/s-ATS-update.js
@@ -2,30 +2,3 @@ var $has_teacher_requested = $("#request-open");
 setInterval(function () {
     $has_teacher_requested.load("s-ATS-request.php #main");
 }, 30000);
-
-var fetch = true;
-var url = 'https://technoboard-extension.000webhostapp.com/ATS/php/student/s-ATS-search-request.php';
-$.ajax(
-{
-    // Post the variable fetch to url.
-    type : 'get',
-    url : url,
-    dataType : 'html', // expected returned data format.
-    data : 
-    {
-        'checkpublish' : fetch // You might want to indicate what you're requesting.
-    },
-    success : function(data)
-    {
-        // This happens AFTER the backend has returned an JSON array (or other object type)
-        var res1, res2;
-        var publish = (document.getElementById("publsih")).innerText;
-        if(publish=="True"){
-            
-        }
-    },
-    complete : function(data)
-    {
-        // do something, not critical.
-    }
-});
